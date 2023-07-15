5e7580c96d507fe9d844cb0890b205e2359fa594
ifernandezatsistemas <ifernandez@atsistemas.com>
2023-07-15 15:55:53 +0200

    Initial Commit

---
+++ interviewapp/interviewappApp.swift
@@ -0,0 +1,17 @@
+//
+//  interviewappApp.swift
+//  interviewapp
+//
+//  Created by Iván Fernández Arenas on 15/7/23.
+//
+
+import SwiftUI
+
+@main
+struct interviewappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
